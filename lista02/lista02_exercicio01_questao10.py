952dd5ba582c37616be39570a52497d2f366396e
VitorSimoes <vitor6427126@gmail.com>
2017-03-15 15:32:06 -0400

    Subindo questão

---
+++ lista02/lista02_exercicio01_questao10.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Natália Cavalcante Xavier  1715310021
+# Antonio Diego Furtado da Silva 1715310004
+# Mateus Oliveira Marques 1515310514
+# Judá Salazar Braga 1515200050
+# Fang Yao 1115180236
+# Vitor Simoes Azevedo 1715310025
+#
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+# ----------------------------------------------------------
+c = float(input('Temperatura em Celsius: '))
+print('A temperatura em Fahrenheit eh: ', (c * 1.8) + 32)
