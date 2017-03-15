1f581c68632d6d0cdcb07ded4a15aac27f7364f7
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-03-14 23:40:02 -0400

    Add files via upload

---
+++ lista02/lista02_exercicio01_questao07.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------
+#Introdução a Programação de Computadores-IPC
+#Profº Jucimar Jr
+#Natália Cavalcante Xavier  1715310021
+#Antonio Diego Furtado da Silva 1715310004
+#Mateus Oliveira Marques 1515310514
+#Judá Salazar Braga 1515200050
+#Fang Yao 1115180236
+#Vitor Simoes Azevedo 1715310025
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+#--------------------------------------------------------------------------------------------------------
+side=float(input("Digite o valor do lado:"))
+area=side**2
+areax2=area*2
+print("O dobro da area é: ",areax2)
